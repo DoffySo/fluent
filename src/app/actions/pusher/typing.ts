6f3acd6337f8d6beb6a6938e7e34c31c55e598b0
DoffySo <bitvapesen@gmail.com>
2025-06-15 18:43:45 +0300

    fix .gitignore

--- src/app/actions/pusher/typing.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { pusherServer } from '@/lib/pusher/server'
-
-export async function sendTyping(chatId: string, userId: string) {
-    await pusherServer.trigger(`chat-${chatId}`, 'typing', { userId })
-}
